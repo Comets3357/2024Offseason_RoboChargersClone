7a4a7479d5621b437b0e03981384d1df435b3569
Minhao Chen <randomrepl@gmail.com>
2024-10-10 18:47:01 -0400

    DriveCommands DriveTo

--- src/main/cpp/Commands/DriveCommands.cpp
+++ src/main/cpp/Commands/DriveCommands.cpp
@@ -1,13 +1,29 @@
 #include "Commands/DriveCommands.h"
 
-using namespace DriveCommands;
+frc2::CommandPtr DriveCommands::StopDrive(DriveSubsystem* drive) {
+   return frc2::cmd::RunOnce([drive] {drive->Drive(0_mps, 0_mps, 0_rad_per_s, true, true);});
+}
 
-frc2::CommandPtr DriveTo(DriveSubsystem* drive, double xPosition, double yPosition) {
+
+frc2::CommandPtr DriveCommands::DriveTo(DriveSubsystem* drive, double xPosition, double yPosition, double rotation = 0) {
    return frc2::FunctionalCommand(
-      [drive] {},
-      [drive] {},
-      [drive] (bool interrupted) {},
-      [drive] {return true;},
+      [drive] {drive->ResetEncoders();},
+      [drive, xPosition, yPosition, rotation] {
+         double MaxAbsValue = std::max(std::abs(xPosition), std::abs(yPosition));
+         units::meters_per_second_t xSpeed{3*(xPosition/MaxAbsValue)};
+         units::meters_per_second_t ySpeed{3*(yPosition/MaxAbsValue)};
+         double magnitude = std::sqrt(std::pow(xPosition, 2) + std::pow(yPosition, 2));
+         double speed = std::sqrt(std::pow(xSpeed.value(), 2) + std::pow(ySpeed.value(), 2));
+         double time = magnitude / speed;
+         units::radians_per_second_t rps{rotation/time};
+         drive->Drive(xSpeed, ySpeed, rps, true, true);
+      },
+      [drive] (bool interrupted) {DriveCommands::StopDrive(drive);},
+      [drive, xPosition, yPosition] {
+         double xCurPos = drive->GetPose().X().value();
+         double yCurPos = drive->GetPose().Y().value();
+         return std::floor(xCurPos) == xPosition && std::floor(yCurPos) == yPosition;
+      },
       {drive}
    ).ToPtr();
 }
--- src/main/include/Commands/DriveCommands.h
+++ src/main/include/Commands/DriveCommands.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <frc2/command/CommandPtr.h>
+#include <frc2/command/Commands.h>
 #include <frc2/command/FunctionalCommand.h>
 #include "Subsystems/DriveSubsystem.h"
 
@@ -11,6 +12,8 @@ namespace DriveCommands {
    * 
    * @param xPosition X Position to drive to
    * @param yPosition Y Position to drive to
+   * @param rotation How much to rotate in radians
    */
-   frc2::CommandPtr DriveTo(DriveSubsystem* drive, double xPosition, double yPosition);
+   frc2::CommandPtr DriveTo(DriveSubsystem* drive, double xPosition, double yPosition, double rotation);
+   frc2::CommandPtr StopDrive(DriveSubsystem* drive);
 }
