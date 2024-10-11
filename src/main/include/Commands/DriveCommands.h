64399b1b3821e2fe4bf4ddfc8bcd2ff291d304b4
Minhao Chen <randomrepl@gmail.com>
2024-10-11 09:55:31 -0400

    Added a single asterisk to comments on DriveCommands.h

--- src/main/include/Commands/DriveCommands.h
+++ src/main/include/Commands/DriveCommands.h
@@ -5,7 +5,7 @@
 #include <frc2/command/FunctionalCommand.h>
 #include "Subsystems/DriveSubsystem.h"
 
-/*
+/**
  * Drives to a certain position from the current position(Opponent starts Jamaican Smiling(those who know(only balkans understand(from the screen to the ring to the pen to the king))))
  *
  * @param xPosition X Position to drive to
