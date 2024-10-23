78c4a34eaadb1becd7de6e3f17366504b8d82edb
Dhanush <mr.dhanush1234@gmail.com>
2024-10-23 19:48:23 -0400

    Led subsystem2 (#15)

---
+++ src/main/cpp/Subsystems/LEDSubystem.cpp
@@ -0,0 +1,54 @@
+#include "Subsystems/LEDSubsystem.h"
+#include <frc/DriverStation.h>
+
+LEDSubsystem::LEDSubsystem(IndexerSubsystem* indexer) {
+  indexerSubsystem = indexer;
+  led.SetLength(90);
+  led.Start();
+  for (size_t i = 0; i < 90; i++)
+  {
+    ledBuffer[i].SetRGB(255, 0, 0);
+  }
+  led.SetData(ledBuffer);
+}
+
+void LEDSubsystem::Periodic() {
+  enabled = frc::DriverStation::IsEnabled();
+  comms = frc::DriverStation::IsDSAttached();
+
+  if (!enabled)
+  {
+    if (comms)
+    {
+      for (size_t i = 0; i < 90; i++)
+      {
+        ledBuffer[i].SetRGB(255, 0, 0);
+      }
+      led.SetData(ledBuffer);
+    } else {
+      for (size_t i = 0; i < 90; i++)
+      {
+        ledBuffer[i].SetRGB(0, 255, 0);
+      }
+      led.SetData(ledBuffer);
+    }
+  } else {
+    if (indexerSubsystem->GamePieceDetected()) {
+      for (size_t i = 0; i < 90; i++)
+      {
+        ledBuffer[i].SetRGB(255, 0, 0);
+      }
+      led.SetData(ledBuffer);
+    } else {
+      for (size_t i = 0; i < 90; i++)
+      {
+        ledBuffer[i].SetRGB(165, 255, 0);
+      }
+      led.SetData(ledBuffer);
+    }
+  }
+}
+
+void LEDSubsystem::SimulationPeriodic() {
+   
+}
